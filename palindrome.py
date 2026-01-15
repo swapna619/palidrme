db865088484e1fb56010d51a9e8017a8d84e8493
swapna619 <swapnarangain619@gmail.com>
2026-01-15 15:34:37 +0530

    palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,32 @@
+# palindrome_check.py
+# Program to check whether a string is a palindrome
+
+import sys
+
 def is_palindrome(text):
-    text = text.lower().replace(" ", "")
-    return text == text[::-1]
+    """Function to check palindrome"""
+    cleaned_text = text.lower().replace(" ", "")
+    return cleaned_text == cleaned_text[::-1]
 
-# Predefined string
-text = "hello"
 
-# Check palindrome
-if is_palindrome(text):
-    print("The given string is a Palindrome")
-else:
-    print("The given string is NOT a Palindrome")
+if __name__ == "__main__":
+    print("\n---- Palindrome Checker ----\n")
+    try:
+        # Case 1: Argument passed (for Jenkins or CLI)
+        if len(sys.argv) == 2:
+            text = sys.argv[1]
+
+        # Case 2: No argument passed (for console use)
+        else:
+            text = input("Enter a string: ")
+
+        result = is_palindrome(text)
+
+        print(f"\nInput Text: {text}")
+        if result:
+            print("Result: The given string is a Palindrome\n")
+        else:
+            print("Result: The given string is NOT a Palindrome\n")
+
+    except Exception as e:
+        print("Error occurred:", e)
