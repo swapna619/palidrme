eca4e78a99fcfc71279eb8c6be1cc9eb106f3af9
swapna619 <swapnarangain619@gmail.com>
2026-01-15 11:11:39 +0530

    palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,22 +1,12 @@
-import sys
-
 def is_palindrome(text):
     text = text.lower().replace(" ", "")
     return text == text[::-1]
 
-def main():
-    # Jenkins / Command-line execution
-    if len(sys.argv) == 2:
-        text = sys.argv[1]
+# Predefined string
+text = "hello"
 
-    # Local interactive execution
-    else:
-        text = input("Enter a string: ")
-
-    if is_palindrome(text):
-        print("The given string is a Palindrome")
-    else:
-        print("The given string is NOT a Palindrome")
-
-if __name__ == "__main__":
-    main()
+# Check palindrome
+if is_palindrome(text):
+    print("The given string is a Palindrome")
+else:
+    print("The given string is NOT a Palindrome")
